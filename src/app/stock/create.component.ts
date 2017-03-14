a46d8dad04d7b42a28cadc60fce62df9b25320fb
lm <yume1@live.cn>
2017-03-14 12:27:36 +0800

    fix sth

--- src/app/stock/create.component.ts
+++ src/app/stock/create.component.ts
@@ -4,36 +4,28 @@ import { GoodsService }    from './goods.service';
 import { Goods }           from '../type';
 
 @Component({
+  selector: 'modal',
   template: `
     <div class="modal-header">
-      <h4 class="modal-title">title</h4>
-      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-        <span aria-hidden="true">&times;</span>
+      <p class="modal-title">title</p>
+      <button type="button" class="modal-close" aria-label="Close" (click)="dismiss()">
+        <i class="fa fa-times" aria-hidden="true"></i>
       </button>
     </div>
     <div class="modal-body">
-      <form>
-        <div class="form-group">
-          <label class="form-control-label" for="name">名称</label>
-          <input type="text" class="form-control" id="name" (ngModel)="item.name">
-        </div>
-        <div class="form-group">
-          <label class="form-control-label" for="quantity">数量</label>
-          <input type="text" class="form-control" id="quantity" (ngModel)="item.quantity">
-        </div>
-      </form>
     </div>
     <div class="modal-footer">
       <button type="button" class="btn btn-secondary" (click)="create()">保存</button>
     </div>
-  `
+  `,
+  styles: [``]
 })
 
 export class CreateItemComponent {
   item:Goods;
   constructor( private goodsService:GoodsService) {}
-/*
-  create():void {
-    this.goodsService.update(this.item).then(() => this.activeModal.close('Close click'));
-  }*/
+
+  create() {
+    
+  }
 }
